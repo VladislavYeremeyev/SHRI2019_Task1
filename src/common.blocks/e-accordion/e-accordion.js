d99a0cff62f79fdc4296abb888dc889ab56eb7d4
Vladislav Yeremeyev <vladislav.eremeev@uchi.ru>
2019-07-20 22:16:19 +0300

    onoffswitch logic added

--- src/common.blocks/e-accordion/e-accordion.js
+++ src/common.blocks/e-accordion/e-accordion.js
@@ -1,18 +1,24 @@
-document.body.addEventListener("click", function(e) {
-  let { target } = e;
+if (document) {
+  document.body.addEventListener("click", function(e) {
+    let { target } = e;
 
-  while (target !== this) {
-    if (target.classList.contains("e-accordion__short")) {
-      let parent = target.closest(".e-accordion");
-      let hiddenPart = parent.getElementsByClassName("e-accordion__more");
-      if (hiddenPart.length === 0) {
+    while (target !== this) {
+      if (target.classList.contains("e-accordion__short")) {
+        let parent = target.closest(".e-accordion");
+
+        if (!parent) {
+          return;
+        }
+        let hiddenPart = parent.getElementsByClassName("e-accordion__more");
+        if (hiddenPart.length === 0) {
+          return;
+        }
+        hiddenPart[0].classList.toggle("e-accordion__more_displayed");
+
         return;
       }
-      hiddenPart[0].classList.toggle("e-accordion__more_displayed");
 
-      return;
+      target = target.parentNode;
     }
-
-    target = target.parentNode;
-  }
-});
+  });
+}
